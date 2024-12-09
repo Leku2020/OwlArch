8dbbca2cff83b445a454cf830756ab3446b0af27
Jorge Lekuona <43331910+Leku2020@users.noreply.github.com>
2024-12-09 20:33:22 +0100

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,1 +1,28 @@
+#!/bin/bash
+set -e
 
+# Configuración básica de Arch
+echo "Configuring OwlArch..."
+timedatectl set-ntp true
+ln -sf /usr/share/zoneinfo/UTC /etc/localtime
+hwclock --systohc
+
+# Idioma y teclado
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
+echo "KEYMAP=us" > /etc/vconsole.conf
+locale-gen
+
+# Nombre del host
+echo "owlArch" > /etc/hostname
+cat <<EOF > /etc/hosts
+127.0.0.1   localhost
+::1         localhost
+127.0.1.1   arch-vm.localdomain arch-vm
+EOF
+
+# Instalación mínima
+pacstrap /mnt base linux linux-firmware
+genfstab -U /mnt >> /mnt/etc/fstab
+arch-chroot /mnt <<EOFCHROOT
+pacman -Syu --noconfirm
+EOFCHROOT
