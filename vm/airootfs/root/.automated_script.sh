1b1a92357e3c92a62d93da123059b19fd672c063
Edu-RB <mrhackersman@pm.me>
2025-03-20 21:16:46 +0100

    adding vm testN 35212

---
+++ vm/airootfs/root/.automated_script.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+script_cmdline() {
+    local param
+    for param in $(</proc/cmdline); do
+        case "${param}" in
+            script=*)
+                echo "${param#*=}"
+                return 0
+                ;;
+        esac
+    done
+}
+
+automated_script() {
+    local script rt
+    script="$(script_cmdline)"
+    if [[ -n "${script}" && ! -x /tmp/startup_script ]]; then
+        if [[ "${script}" =~ ^((http|https|ftp|tftp)://) ]]; then
+            # there's no synchronization for network availability before executing this script
+            printf '%s: waiting for network-online.target\n' "$0"
+            until systemctl --quiet is-active network-online.target; do
+                sleep 1
+            done
+            printf '%s: downloading %s\n' "$0" "${script}"
+            curl "${script}" --location --retry-connrefused --retry 10 --fail -s -o /tmp/startup_script
+            rt=$?
+        else
+            cp "${script}" /tmp/startup_script
+            rt=$?
+        fi
+        if [[ ${rt} -eq 0 ]]; then
+            chmod +x /tmp/startup_script
+            printf '%s: executing automated script\n' "$0"
+            # note that script is executed when other services (like pacman-init) may be still in progress, please
+            # synchronize to "systemctl is-system-running --wait" when your script depends on other services
+            /tmp/startup_script
+        fi
+    fi
+    local userconfig_script="/airootfs/root/userconfig_script.sh"  # Path to the second script
+    if [[ -f "${userconfig_script}" && ! -x /tmp/userconfig_script.sh ]]; then
+        cp "${userconfig_script}" /tmp/userconfig_script.sh
+        chmod +x /tmp/userconfig_script.sh
+        printf '%s: executing userconfig_script.sh\n' "$0" "${userconfig_script}"
+        /tmp/userconfig_script.sh
+    fi
+}
+
+if [[ $(tty) == "/dev/tty1" ]]; then
+    automated_script
+fi
---
+++ vm/compile.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+sudo rm -r work/
+sudo rm -r out/
+mkdir work
+mkdir out
+ln -s /usr/lib/systemd/system/vboxservice.service /home/edu/new/OwlArch/vm/airootfs/etc/systemd/system/multi-user.target.wants/vboxservice.service
+ln -sf /usr/lib/systemd/system/gdm.service /home/edu/new/OwlArch/vm/airootfs/etc/systemd/system/display-manager.service
+ln -sf /usr/lib/systemd/system/NetworkManager.service /home/edu/new/OwlArch/vm/airootfs/etc/systemd/system/multi-user.target.wants/NetworkManager.service
+ln -sf /usr/lib/systemd/system/dhcpcd.service /home/edu/new/OwlArch/vm/airootfs/etc/systemd/system/multi-user.target.wants/dhcpcd.service
+sudo mkarchiso -v -w work -o out "/home/edu/new/OwlArch/vm"
