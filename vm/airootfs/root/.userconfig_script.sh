da7b6153152c3572910fb8135b9aff58afeff45b
Edu-RB <mrhackersman@pm.me>
2025-03-21 19:46:51 +0100

    Update .userconfig_script.sh

--- vm/airootfs/root/.userconfig_script.sh
+++ vm/airootfs/root/.userconfig_script.sh
@@ -10,13 +10,10 @@ sudo iptables -A INPUT -p udp --dport 1194 -j ACCEPT
 
 # Allow SSH (port 22) for remote connections
 iptables -A OUTPUT -m owner --uid-owner analyst -p tcp --dport 22 -j ACCEPT
-iptables -A INPUT -m owner --uid-owner analyst -p tcp --sport 22 -j ACCEPT
 
 # Allow HTTP (port 80) and HTTPS (port 443) for web browsing
 iptables -A OUTPUT -m owner --uid-owner analyst -p tcp --dport 80 -j ACCEPT
 iptables -A OUTPUT -m owner --uid-owner analyst -p tcp --dport 443 -j ACCEPT
-iptables -A INPUT -m owner --uid-owner analyst -p tcp --sport 80 -j ACCEPT
-iptables -A INPUT -m owner --uid-owner analyst -p tcp --sport 443 -j ACCEPT
 
 # Allow DNS (port 53) for name resolution
 iptables -A OUTPUT -m owner --uid-owner analyst -p udp --dport 53 -j ACCEPT
@@ -27,38 +24,34 @@ iptables -A OUTPUT -p udp --dport 123 -j ACCEPT
 
 #HUNTER configuration
 # Radare2 web interface
-iptables -A INPUT -p tcp --dport 8000 -m owner --uid-owner hunter -j ACCEPT
 iptables -A OUTPUT -p tcp --dport 8000 -m owner --uid-owner hunter -j ACCEPT
 
 # Ghidra server mode
-iptables -A INPUT -p tcp --dport 13100 -m owner --uid-owner hunter -j ACCEPT
 iptables -A OUTPUT -p tcp --dport 13100 -m owner --uid-owner hunter -j ACCEPT
 
 # Cuckoo Web interface
-iptables -A INPUT -p tcp --dport 2042 -m owner --uid-owner hunter -j ACCEPT
 iptables -A OUTPUT -p tcp --dport 2042 -m owner --uid-owner hunter -j ACCEPT
 
 # Cuckoo VM management
-iptables -A INPUT -p tcp --dport 5900 -m owner --uid-owner hunter -j ACCEPT
 iptables -A OUTPUT -p tcp --dport 5900 -m owner --uid-owner hunter -j ACCEPT
 
 # Cuckoo server-worker communication
-iptables -A INPUT -p tcp --dport 10001 -m owner --uid-owner hunter -j ACCEPT
 iptables -A OUTPUT -p tcp --dport 10001 -m owner --uid-owner hunter -j ACCEPT
 
 # SURICATA default ports
-iptables -A INPUT -p tcp --dport 31337 -m owner --uid-owner hunter -j ACCEPT
 iptables -A OUTPUT -p tcp --dport 31337 -m owner --uid-owner hunter -j ACCEPT
+
+# FRIDA default ports
+iptables -A OUTPUT -p tcp --dport 27042 -m owner --uid-owner hunter -j ACCEPT
 
 # Block all other outgoing and incoming traffic
 iptables -A OUTPUT -m owner --uid-owner hunter -j DROP
-iptables -A INPUT -m owner --uid-owner hunter -j DROP
 
-#ANALYST configuration
+#ANALYST configuration is the base configuration, with the rest of the ports closed.
+#OwlArch will have all ports open for ease of administration tasks.
 
 # Block all other outgoing and incoming traffic
 iptables -A OUTPUT -m owner --uid-owner analyst -j DROP
-iptables -A INPUT -m owner --uid-owner analyst -j DROP
 
 #enabling persistency after boot
 iptables-save > /etc/iptables/iptables.rules
