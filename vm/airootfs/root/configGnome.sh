647264ed6d962d6d7e7493ba368671f668ee0dcd
Jorge Lekuona <43331910+Leku2020@users.noreply.github.com>
2025-03-26 23:04:49 +0100

    Background

--- vm/airootfs/root/configGnome.sh
+++ vm/airootfs/root/configGnome.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-gsettings set org.gnome.desktop.background picture-uri "file:///usr/share/backgrounds/gnome/background.png"
-
+# settings set org.gnome.desktop.background picture-uri "file:///usr/share/backgrounds/gnome/background.png"
+cp usr/local/glib-2.0/schemas/org.gnome.desktop.background.gschema.xml usr/share/glib-2.0/schemas/org.gnome.desktop.background.gschema.xml
 echo "Fondo de pantalla configurado correctamente."
