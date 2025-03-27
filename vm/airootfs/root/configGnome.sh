763db737274de3f8f3560a1edc7ef0ac60743d82
Jorge Lekuona <43331910+Leku2020@users.noreply.github.com>
2025-03-27 16:56:57 +0100

    Txapu 3.0

--- vm/airootfs/root/configGnome.sh
+++ vm/airootfs/root/configGnome.sh
@@ -2,5 +2,6 @@
 
 # settings set org.gnome.desktop.background picture-uri "file:///usr/share/backgrounds/gnome/background.png"
 cp usr/local/glib-2.0/schemas/org.gnome.desktop.background.gschema.xml usr/share/glib-2.0/schemas/org.gnome.desktop.background.gschema.xml
+mv usr/local/share/backgrounds/gnome/* /usr/share/backgrounds/gnome
 dconf load / < root/gnome-config.txt
 echo "Fondo de pantalla configurado correctamente."
